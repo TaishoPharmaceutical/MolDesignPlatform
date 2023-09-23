4f0e09dcf3321d2efd2ba6c3fd96e85cb0c4f073
Tore Eriksson <tore.eriksson@gmail.com>
2023-09-23 18:25:37 +0200

    List argument in popen

--- run.py
+++ run.py
@@ -17,44 +17,30 @@ if __name__=="__main__":
 
     python = config["python"]
     chemdoodle_api = os.path.join(root_path, "Chemdoodle","ChemDoodleMediator.py")
-    cmd = " ".join([python, chemdoodle_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen([python, chemdoodle_api])
 
     # Run Ensemble Predictor Model
     ensemble_api = os.path.join(root_path, "Ensemble", "PredictionAPI.py")
-    cmd = " ".join([python, ensemble_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen([python, ensemble_api])
 
     # Run FragmentAE
     fragmentAE_api = os.path.join(root_path, "FragmentAE", "FragmentAPI.py")
-    cmd = " ".join([python, fragmentAE_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen([python, fragmentAE_api])
 
     # Run Reactor
     reactor_api = os.path.join(root_path, "Reactor", "ReactorAPI.py")
-    cmd = " ".join([python, reactor_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen([python, reactor_api])
 
     # Run Filter
     tasf_api = os.path.join(root_path, "Filter","filter_api.py")
-    cmd = " ".join([python, tasf_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen([python, tasf_api])
 
     # Run VQVAE
     vqvae_api = os.path.join(root_path, "VQVAE", "ChemGenAPI.py")
-    cmd = " ".join([python, vqvae_api])
-    sp = subprocess.Popen(cmd)
+    sp = subprocess.Popen([python, vqvae_api])
 
-
-    # Run WebGUI
     time.sleep(20)
 
+    # Run WebGUI
     webgui_api = os.path.join(root_path, "WebGUI", "WebGUI_run.py")
-    cmd = " ".join([python, webgui_api])
-    sp = subprocess.Popen(cmd)
-
+    sp = subprocess.Popen()
